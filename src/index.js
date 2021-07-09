9f62de48b07cec38e9dc6ec027569d1e61e28a4c
Higor <hhigorsilvachaves@gmail.com>
2021-07-08 21:53:45 -0300

    feat: oassing on two tests

--- src/index.js
+++ src/index.js
@@ -1,21 +1,48 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  
+  const userAlreadyExists = users.find((user) => user.username === username)
+
+  if(userAlreadyExists) {
+    return response.status(400).json({error: "user already exists"})
+  }
+
+  request.user = userAlreadyExists
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body
+
+  const userAlreadyExists = users.find((user) => user.username === username)
+
+  if(userAlreadyExists) {
+    return response.status(400).json({error: "user already exists"})
+  }
+
+  const newUser = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: [],
+    created_at: new Date()
+  }
+
+  users.push(newUser)
+
+  return response.status(201).json(newUser)
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
